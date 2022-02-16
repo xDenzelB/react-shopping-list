6c9d5854455de55b83bf33ff9b65d592b7a32b8e
Denzel <dbartolaba123@gmail.com>
2022-02-16 15:28:14 -0800

    Added in routes

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ export default function App() {
 
       if (user) setUser(user);
     }
-  })
+
+    fetch();
+  }, []);
 
   async function handleLogout() {
+
+    await logout();
+    setUser(null);
     // call the logout function
     // clear the user in state
   }
@@ -35,14 +40,25 @@ export default function App() {
     <Router>
       <div className='App'>
         <header>
+          {user && <button onClick={handleLogout}>Logout</button>}
           {/* if there's a user, render a logout button here */}
         </header>
         <main>
           <Switch>
             <Route exact path="/">
+              {
+                user
+                  ? <Redirect to='./shopping-list' />
+                  : <AuthPage setUser={setUser} />
+              }
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
             </Route>
             <Route exact path="/shopping-list">
+              {
+                user 
+                  ? <ListPage />
+                  : <Redirect to='/' />
+              }
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
 
             </Route>
