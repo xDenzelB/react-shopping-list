40d1f9dd0fd33d9856ab0f515cc187d072bb4e6c
Denzel <dbartolaba123@gmail.com>
2022-02-16 15:50:20 -0800

    Finishe ListItemForm

--- src/ListItemForm.js
+++ src/ListItemForm.js
@@ -3,13 +3,19 @@ import { createListItem } from './services/fetch-utils';
 
 export default function ListItemForm({ fetchItems }) {
   // you'll need to track the name and quantity in state
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
 
   async function handleSubmit(e) {
     e.preventDefault();
     
     // make a new list item in supabase using the form values stored in state
+    await createListItem({ name, quantity });
 
     // refetch the items using the handler functionpassed down as a prop
+    await fetchItems();
+    setName('');
+    setQuantity('');
 
     // clear the name and quantity in state to refresh the form
   }
@@ -17,12 +23,12 @@ export default function ListItemForm({ fetchItems }) {
   return (
     <div className='new-item-form-container'>
       {/* on submit, call the handleSubmit function */}
-      <form>
+      <form onSubmit={handleSubmit}>
           I need . . . 
         <label>
             Quantity
           {/* on change, update the quantity in state */}
-          <input 
+          <input value={quantity} onChange={e => setQuantity(e.target.value)}
             // this should be a controlled input, soi set the value based on state
             required 
             type="number" 
@@ -32,7 +38,7 @@ export default function ListItemForm({ fetchItems }) {
         <label>
             Name
           {/* on change, update the name in state */}
-          <input
+          <input value={name} onChange={e => setName(e.target.value)}
             // this should be a controlled input, soi set the value based on state 
             required 
             name="name" />
